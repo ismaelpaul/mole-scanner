130a75b6b207193f2dfcf6a88703baa9cf8a9aae
ismaelpaul <ismaafp@gmail.com>
2025-12-30 18:03:20 +0000

    Add camera permissions

--- app/(tabs)/scan.tsx
+++ app/(tabs)/scan.tsx
@@ -1,10 +1,35 @@
+import Camera from '@/components/Camera';
 import { ThemedText } from '@/components/ThemedText';
-import { ScrollView } from 'react-native';
+import { ThemedView } from '@/components/ThemedView';
+import { useCameraPermissions } from 'expo-camera';
+import { useEffect } from 'react';
+import { Alert } from 'react-native';
 
 export default function ScanScreen() {
+	const [cameraPermission, requestCameraPermission] = useCameraPermissions();
+
+	useEffect(() => {
+		async function requestPermissions() {
+			const cameraStatus = await requestCameraPermission();
+
+			if (!cameraStatus?.granted) {
+				Alert.alert(
+					'Permission required',
+					'Camera access is needed to take photos.'
+				);
+				return false;
+			}
+		}
+		requestPermissions();
+	}, []);
+
 	return (
-		<ScrollView>
-			<ThemedText type="title">Welcome to the Scan Screen!</ThemedText>
-		</ScrollView>
+		<ThemedView style={{ flex: 1 }}>
+			{cameraPermission?.granted ? (
+				<Camera />
+			) : (
+				<ThemedText>Please allow camera access to use this feature.</ThemedText>
+			)}
+		</ThemedView>
 	);
 }
