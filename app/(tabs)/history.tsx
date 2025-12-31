93acab816aedc060e02b7b6c3e2968a14b0e5a07
ismaelpaul <ismaafp@gmail.com>
2025-12-31 16:46:42 +0000

    Wrap in Screen component

--- app/(tabs)/history.tsx
+++ app/(tabs)/history.tsx
@@ -1,10 +1,13 @@
+import { Screen } from '@/components/Screen';
 import { ThemedText } from '@/components/ThemedText';
 import { ScrollView } from 'react-native';
 
 export default function HistoryScreen() {
 	return (
-		<ScrollView>
-			<ThemedText type="title">Welcome to the History Screen!</ThemedText>
-		</ScrollView>
+		<Screen>
+			<ScrollView contentContainerStyle={{ padding: 20 }}>
+				<ThemedText type="title">Welcome to the History Screen!</ThemedText>
+			</ScrollView>
+		</Screen>
 	);
 }
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,13 @@
+import { Screen } from '@/components/Screen';
 import { ThemedText } from '@/components/ThemedText';
 import { ScrollView } from 'react-native';
 
 export default function ProfileScreen() {
 	return (
-		<ScrollView>
-			<ThemedText type="title">Welcome to the Profile Screen!</ThemedText>
-		</ScrollView>
+		<Screen>
+			<ScrollView contentContainerStyle={{ padding: 20 }}>
+				<ThemedText type="title">Welcome to the Profile Screen!</ThemedText>
+			</ScrollView>
+		</Screen>
 	);
 }
